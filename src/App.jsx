8e150ba5bc4611f4ad572ad08eee979a3706fa00
SuperPsycho4347 <superpsycho4347@gmail.com>
2025-08-07 22:37:15 +0500

    General: added initial skeleton for general component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { useState } from 'react'
 import './App.css'
+import General from './components/General'
 import Input from './components/Input'
 
 function App() {
+  const [generalInfo, setGeneralInfo] = useState({
+    name: '',
+    email: '',
+    phone: ''
+  })
+
   return (
-    <Input
-      label="Cunt"
-      text="Cunt"
-    />
+    <General/>
   )
 }
 
---
+++ src/components/General.jsx
@@ -0,0 +1,29 @@
+function General(generalInfo) {
+    return(
+        <div className="general-form">
+            <label>
+                Name
+                {''}
+                <input
+                    type="text"
+                />
+            </label>
+            <label>
+                Email
+                {''}
+                <input
+                    type="text"
+                />
+            </label>
+            <label>
+                Phone
+                {''}
+                <input
+                    type="text"
+                />
+            </label>
+        </div>
+    )
+}
+
+export default General
