54b88937eaabdba98d68fd153a5a16e5a3e65b87
SuperPsycho4347 <superpsycho4347@gmail.com>
2025-09-23 16:36:40 +0500

    General: added initial skeleton of a form

---
+++ src/components/General.jsx
@@ -0,0 +1,19 @@
+export default function General() {
+    return (
+        <form>
+            <label>
+                Name:
+                <input type="text" />
+            </label>
+            <label>
+                Email:
+                <input type="email" />
+            </label>
+            <label>
+                Phone:
+                <input type="text" />
+            </label>
+            <button>Submit</button>
+        </form>
+    )
+}
