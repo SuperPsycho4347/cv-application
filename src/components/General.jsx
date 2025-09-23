5981012fc3b57d20f38d7fd5a41cb9070554a1fc
SuperPsycho4347 <superpsycho4347@gmail.com>
2025-09-23 16:51:28 +0500

    General: added state for individual input fields, and event handlers.

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,19 +1,40 @@
+import { useState } from "react";
+
 export default function General() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>
                 Name:
-                <input type="text" />
+                <input type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                />
             </label>
             <label>
                 Email:
-                <input type="email" />
+                <input type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                />
             </label>
             <label>
                 Phone:
-                <input type="text" />
+                <input type="text"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                />
             </label>
-            <button>Submit</button>
+            <button 
+                type="submit"
+            >Submit</button>
         </form>
     )
 }
